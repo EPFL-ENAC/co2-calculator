f7f9bed9fe39580259b68da3ced00e315a5291cf
Guilbert Pierre <pierre.guilbert@epfl.ch>
2025-11-27 21:50:58 +0100

    feat(frontend-module-equipement-electric): add module title component

--- frontend/src/pages/app/ModulePage.vue
+++ frontend/src/pages/app/ModulePage.vue
@@ -1,7 +1,21 @@
 <template>
-  <q-page>
-    <h1 class="text-h1">Module</h1>
+  <q-page class="module-page">
+    <module-title :type="currentModuleType" />
   </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useRoute } from 'vue-router';
+import { computed } from 'vue';
+
+import ModuleTitle from 'src/components/organisms/module/ModuleTitle.vue';
+const $route = useRoute();
+const currentModuleType = computed(() => $route.params.module as string);
+</script>
+
+<style scoped lang="scss">
+@use 'src/css/02-tokens' as tokens;
+.module-page {
+  padding-top: tokens.$layout-page-padding-y;
+}
+</style>
