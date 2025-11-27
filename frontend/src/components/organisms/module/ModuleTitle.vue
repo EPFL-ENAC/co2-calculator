0a2f6c6012eec2f739263d27fa2250ac06c1c8b9
Guilbert Pierre <pierre.guilbert@epfl.ch>
2025-11-27 21:50:58 +0100

    feat(frontend-module-equipement-electric): add module title component

---
+++ frontend/src/components/organisms/module/ModuleTitle.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-card flat class="container">
+    <q-card-section>
+      <h1 class="text-h2 q-mb-xs">
+        <q-icon
+          :name="MODULE_ICONS[props.type] ? MODULE_ICONS[props.type] : ''"
+          color="accent"
+          size="sm"
+        ></q-icon>
+        {{ $t(`${props.type}`) }}
+      </h1>
+      <p class="text-body1 q-mb-none">
+        {{ $t(`${props.type}-description`) }}
+      </p>
+      <div class="absolute-top-right q-mr-md q-mt-md">
+        <q-icon :name="outlinedInfo" size="md" class="cursor-pointer" />
+        <!-- tooltip width should be token related -->
+        <q-tooltip
+          anchor="top right"
+          self="bottom right"
+          class="u-tooltip-max-width"
+        >
+          {{ $t(`${props.type}-title-tooltip`) }}
+        </q-tooltip>
+      </div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup lang="ts">
+import { MODULE_ICONS } from 'src/constant/moduleCards';
+import { outlinedInfo } from '@quasar/extras/material-icons-outlined';
+
+const props = defineProps<{
+  type: string;
+}>();
+</script>
