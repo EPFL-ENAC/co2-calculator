86bf131ab0d6644fb27444f3f6c19ac7ecff8c71
ambroise-dly <ambroise.delalyproton.me@proton.me>
2025-11-28 09:53:09 +0100

    feat: standardize translations across i18n files for consistency

--- frontend/src/i18n/equipmentelectricconsumption.ts
+++ frontend/src/i18n/equipmentelectricconsumption.ts
@@ -1,12 +1,12 @@
 import { MODULES, MODULES_DESCRIPTIONS } from 'src/constant/modules';
 
 export default {
-    [MODULES.EquipmentElectricConsumption]: {
-        en: 'Equipments',
-        fr: 'Equipment Electricity Consumption',
-    },
-    [MODULES_DESCRIPTIONS.EquipmentElectricConsumption]: {
-        en: 'List lab equipment with wattage to calculate electricity-related CO₂',
-        fr: 'List laboratory equipment with their power to calculate CO₂ related to electricity',
-    },
+  [MODULES.EquipmentElectricConsumption]: {
+    en: 'Equipments',
+    fr: 'Equipment Electricity Consumption',
+  },
+  [MODULES_DESCRIPTIONS.EquipmentElectricConsumption]: {
+    en: 'List lab equipment with wattage to calculate electricity-related CO₂',
+    fr: 'List laboratory equipment with their power to calculate CO₂ related to electricity',
+  },
 } as const;
--- frontend/src/i18n/purchase.ts
+++ frontend/src/i18n/purchase.ts
@@ -1,12 +1,12 @@
 import { MODULES, MODULES_DESCRIPTIONS } from 'src/constant/modules';
 
 export default {
-    [MODULES.Purchase]: {
-        en: 'Purchases',
-        fr: 'Purchases',
-    },
-    [MODULES_DESCRIPTIONS.Purchase]: {
-        en: 'Input annual purchases to assess supply chain emissions footprint',
-        fr: 'Enter annual purchases to assess the supply chain footprint',
-    },
+  [MODULES.Purchase]: {
+    en: 'Purchases',
+    fr: 'Purchases',
+  },
+  [MODULES_DESCRIPTIONS.Purchase]: {
+    en: 'Input annual purchases to assess supply chain emissions footprint',
+    fr: 'Enter annual purchases to assess the supply chain footprint',
+  },
 } as const;
