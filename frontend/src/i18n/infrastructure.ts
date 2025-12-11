020a1d7357a8cc78b21cb88d2308bd32ce322dae
Charlotte Weil <charlotte.weil@epfl.ch>
2025-12-11 15:52:36 +0100

    Change French translation of 'Infrastructure'
    Updated French translation for 'Infrastructure'.

--- frontend/src/i18n/infrastructure.ts
+++ frontend/src/i18n/infrastructure.ts
@@ -3,7 +3,7 @@ import { MODULES, MODULES_DESCRIPTIONS } from 'src/constant/modules';
 export default {
   [MODULES.Infrastructure]: {
     en: 'Infrastructure',
-    fr: 'Infrastructure',
+    fr: 'Infrastructures',
   },
   [MODULES_DESCRIPTIONS.Infrastructure]: {
     en: "Define your lab's physical footprint across EPFL buildings and spaces.",
