9b0fe0b5732f8b44b8e0e63f0cd1f88633fdc154
ambroise-dly <ambroise.delalyproton.me@proton.me>
2026-01-15 09:30:28 +0100

    feat: add link and change texts

--- frontend/src/i18n/backoffice_documentation_editing.ts
+++ frontend/src/i18n/backoffice_documentation_editing.ts
@@ -4,7 +4,7 @@ export default {
     fr: 'Traduction',
   },
   documentation_editing_translation_description: {
-    en: 'Link to the translation files in the repository of the CO₂ calculator application. Make your changes directly on GitHub and ask for a Pull Request. For more information see the backoffice documentation section; Changing texts.',
+    en: 'Link to the translation files in the repository of the CO₂ calculator application. Make your changes directly on GitHub and ask for a Pull Request. For more information see the backoffice documentation section; Changing translation.',
     fr: 'Lien vers les fichiers de traduction dans le repository de l’application Calculateur CO₂. Effectuez vos modifications directement sur GitHub et demandez une Pull Request. Pour plus d’informations, consultez la section documentation du back-office ; Modifier les textes.',
   },
   documentation_editing_translation_description_part_1: {
@@ -12,12 +12,12 @@ export default {
     fr: 'Lien vers les fichiers de traduction dans le repository de l’application Calculateur CO₂. Effectuez vos modifications directement sur GitHub et demandez une Pull Request. Pour plus d’informations, consultez la section documentation du back-office ; ',
   },
   documentation_editing_translation_description_link_text: {
-    en: 'Changing texts',
-    fr: 'Modifier les textes',
+    en: 'Update Translation',
+    fr: 'Mettre à jour les traductions',
   },
   documentation_editing_translation_description_link_url: {
-    en: 'https://epfl-enac.github.io/co2-calculator-back-office-doc/changetext/',
-    fr: 'https://epfl-enac.github.io/co2-calculator-back-office-doc/changetext/',
+    en: 'https://epfl-enac.github.io/co2-calculator-back-office-doc/translation-update/',
+    fr: 'https://epfl-enac.github.io/co2-calculator-back-office-doc/translation-update/',
   },
   documentation_editing_translation_description_part_2: {
     en: '.',
@@ -27,9 +27,21 @@ export default {
     en: 'Documentation',
     fr: 'Documentation',
   },
-  documentation_editing_documentation_description: {
-    en: 'Link to the different documentation repository of the CO₂ calculator application. Make your changes directly on GitHub and ask for a Pull Request. For more information see the backoffice documentation section; Change documentation.',
-    fr: 'Lien vers les différents repository de documentation de l’application Calculateur CO₂. Effectuez vos modifications directement sur GitHub et demandez une Pull Request. Pour plus d’informations, consultez la section documentation du back-office ; Modifier la documentation.',
+  documentation_editing_documentation_description_part_1: {
+    en: 'Link to the different documentation repository of the CO₂ calculator application. Make your changes directly on GitHub and ask for a Pull Request. For more information see the backoffice documentation section; ',
+    fr: 'Lien vers les différents repository de documentation de l’application Calculateur CO₂. Effectuez vos modifications directement sur GitHub et demandez une Pull Request. Pour plus d’informations, consultez la section documentation du back-office ; ',
+  },
+  documentation_editing_documentation_description_link_text: {
+    en: 'Update Documentation',
+    fr: 'Mettre à jour la documentation',
+  },
+  documentation_editing_documentation_description_link_url: {
+    en: 'https://epfl-enac.github.io/co2-calculator-back-office-doc/doc-update/',
+    fr: 'https://epfl-enac.github.io/co2-calculator-back-office-doc/doc-update/',
+  },
+  documentation_editing_documentation_description_part_2: {
+    en: '.',
+    fr: '.',
   },
   documentation_editing_calculator_user_documentation_title: {
     en: 'User Documentation',
@@ -86,143 +98,143 @@ export default {
     fr: 'Commun',
   },
   documentation_editing_rows_common_description: {
-    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque arcu, ac semper tortor posuere vel.',
-    fr: "Texte d'exemple en français. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    en: 'Find all common text used throughout the application, such as headers, footers, and navigation elements.',
+    fr: "Trouvez tous les textes communs utilisés dans toute l'application, telles que les en-têtes, les pieds de page et les éléments de navigation.",
   },
   documentation_editing_rows_my_lab_topic: {
     en: 'Headcount',
     fr: 'Personnel',
   },
   documentation_editing_rows_my_lab_description: {
-    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque arcu, ac semper tortor posuere vel.',
-    fr: "Texte d'exemple en français. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    en: 'Find all text related to managing headcount and personnel information within the application.',
+    fr: "Trouvez tous les textes liés à la gestion du personnel et des informations sur le personnel dans l'application.",
   },
   documentation_editing_rows_professional_travel_topic: {
     en: 'Professional Travel',
     fr: 'Voyages professionnels',
   },
   documentation_editing_rows_professional_travel_description: {
-    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque arcu, ac semper tortor posuere vel.',
-    fr: "Texte d'exemple en français. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    en: 'Find all text related to professional travel within the application.',
+    fr: "Trouvez tous les textes liés aux voyages professionnels dans l'application.",
   },
   documentation_editing_rows_infrastructure_topic: {
     en: 'Infrastructure',
     fr: 'Infrastructures',
   },
   documentation_editing_rows_infrastructure_description: {
-    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque arcu, ac semper tortor posuere vel.',
-    fr: "Texte d'exemple en français. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    en: 'Find all text related to infrastructure within the application.',
+    fr: "Trouvez tous les textes liés aux infrastructures dans l'application.",
   },
   documentation_editing_rows_equipment_electric_consumption_topic: {
     en: 'Equipment Electric Consumption',
     fr: 'Consommation Électrique des Équipements',
   },
   documentation_editing_rows_equipment_electric_consumption_description: {
-    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque arcu, ac semper tortor posuere vel.',
-    fr: "Texte d'exemple en français. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    en: 'Find all text related to equipment electric consumption within the application.',
+    fr: "Trouvez tous les textes liés à la consommation électrique des équipements dans l'application.",
   },
   documentation_editing_rows_purchase_topic: {
     en: 'Purchase',
     fr: 'Achats',
   },
   documentation_editing_rows_purchase_description: {
-    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque arcu, ac semper tortor posuere vel.',
-    fr: "Texte d'exemple en français. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    en: 'Find all text related to purchases within the application.',
+    fr: "Trouvez tous les textes liés aux achats dans l'application.",
   },
   documentation_editing_rows_internal_services_topic: {
     en: 'Internal Services',
     fr: 'Services Internes',
   },
   documentation_editing_rows_internal_services_description: {
-    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque arcu, ac semper tortor posuere vel.',
-    fr: "Texte d'exemple en français. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    en: 'Find all text related to internal services within the application.',
+    fr: "Trouvez tous les textes liés aux services internes dans l'application.",
   },
   documentation_editing_rows_external_cloud_topic: {
     en: 'External Cloud',
     fr: 'Cloud Externe',
   },
   documentation_editing_rows_external_cloud_description: {
-    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque arcu, ac semper tortor posuere vel.',
-    fr: "Texte d'exemple en français. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    en: 'Find all text related to external cloud within the application.',
+    fr: "Trouvez tous les textes liés au cloud externe dans l'application.",
   },
   documentation_editing_rows_backoffice_topic: {
     en: 'Backoffice',
     fr: 'Backoffice',
   },
   documentation_editing_rows_backoffice_description: {
-    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque arcu, ac semper tortor posuere vel.',
-    fr: "Texte d'exemple en français. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    en: 'Find all text related to backoffice within the application.',
+    fr: "Trouvez tous les textes liés au backoffice dans l'application.",
   },
   documentation_editing_rows_admin_topic: {
     en: 'Admin',
     fr: 'Admin',
   },
   documentation_editing_rows_admin_description: {
-    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque arcu, ac semper tortor posuere vel.',
-    fr: "Texte d'exemple en français. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    en: 'Find all text related to admin within the application.',
+    fr: "Trouvez tous les textes liés à l'administration dans l'application.",
   },
   documentation_editing_rows_backoffice_data_management_topic: {
     en: 'Backoffice Data Management',
     fr: 'Gestion des Données Backoffice',
   },
   documentation_editing_rows_backoffice_data_management_description: {
-    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque arcu, ac semper tortor posuere vel.',
-    fr: "Texte d'exemple en français. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    en: 'Find all text related to backoffice data management within the application.',
+    fr: "Trouvez tous les textes liés à la gestion des données backoffice dans l'application.",
   },
   documentation_editing_rows_backoffice_documentation_editing_topic: {
     en: 'Backoffice Documentation Editing',
     fr: 'Édition de la Documentation Backoffice',
   },
   documentation_editing_rows_backoffice_documentation_editing_description: {
-    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque arcu, ac semper tortor posuere vel.',
-    fr: "Texte d'exemple en français. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    en: 'Find all text related to backoffice documentation editing within the application.',
+    fr: "Trouvez tous les textes liés à l'édition de la documentation backoffice dans l'application.",
   },
   documentation_editing_rows_backoffice_reporting_topic: {
     en: 'Backoffice Reporting',
     fr: 'Rapports Backoffice',
   },
   documentation_editing_rows_backoffice_reporting_description: {
-    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque arcu, ac semper tortor posuere vel.',
-    fr: "Texte d'exemple en français. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    en: 'Find all text related to backoffice reporting within the application.',
+    fr: "Trouvez tous les textes liés aux rapports backoffice dans l'application.",
   },
   documentation_editing_rows_backoffice_user_management_topic: {
     en: 'Backoffice User Management',
     fr: 'Gestion des Utilisateurs Backoffice',
   },
   documentation_editing_rows_backoffice_user_management_description: {
-    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque arcu, ac semper tortor posuere vel.',
-    fr: "Texte d'exemple en français. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    en: 'Find all text related to backoffice user management within the application.',
+    fr: "Trouvez tous les textes liés à la gestion des utilisateurs backoffice dans l'application.",
   },
   documentation_editing_rows_home_topic: {
     en: 'Home',
     fr: 'Accueil',
   },
   documentation_editing_rows_home_description: {
-    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque arcu, ac semper tortor posuere vel.',
-    fr: "Texte d'exemple en français. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    en: 'Find all text related to home within the application.',
+    fr: "Trouvez tous les textes liés à l'accueil dans l'application.",
   },
   documentation_editing_rows_login_topic: {
     en: 'Login',
     fr: 'Connexion',
   },
   documentation_editing_rows_login_description: {
-    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque arcu, ac semper tortor posuere vel.',
-    fr: "Texte d'exemple en français. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    en: 'Find all text related to login within the application.',
+    fr: "Trouvez tous les textes liés à la connexion dans l'application.",
   },
   documentation_editing_rows_results_topic: {
     en: 'Results',
     fr: 'Résultats',
   },
   documentation_editing_rows_results_description: {
-    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque arcu, ac semper tortor posuere vel.',
-    fr: "Texte d'exemple en français. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    en: 'Find all text related to results within the application.',
+    fr: "Trouvez tous les textes liés aux résultats dans l'application.",
   },
   documentation_editing_rows_workspace_setup_topic: {
     en: 'Workspace Setup',
     fr: 'Changer de Laboratoire',
   },
   documentation_editing_rows_workspace_setup_description: {
-    en: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque aliquet scelerisque arcu, ac semper tortor posuere vel.',
-    fr: "Texte d'exemple en français. Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    en: 'Find all text related to workspace setup within the application.',
+    fr: "Trouvez tous les textes liés au changement de laboratoire dans l'application.",
   },
 } as const;
