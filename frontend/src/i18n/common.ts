d6942aa3caf9717e949282b7d1100fb6930b756b
ambroise-dly <ambroise.delaly@epfl.ch>
2026-01-23 16:23:29 +0100

    text: chnage text of Doc

--- frontend/src/i18n/common.ts
+++ frontend/src/i18n/common.ts
@@ -193,8 +193,8 @@ export default {
     fr: 'Documentation',
   },
   documentation_backoffice_button_label: {
-    en: 'Guidance',
-    fr: 'Guide',
+    en: 'Back-office Guidance',
+    fr: 'Back-office Guide',
   },
   documentation_dev_button_label: {
     en: 'Developer Documentation',
