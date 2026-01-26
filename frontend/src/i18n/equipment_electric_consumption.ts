087b30b30f96f3e6f45c6254d868a7c8a6d15271
Charlotte Weil <charlotte.weil@epfl.ch>
2026-01-26 13:13:17 +0100

    Replace @ by [at] in frontend texts

--- frontend/src/i18n/equipment_electric_consumption.ts
+++ frontend/src/i18n/equipment_electric_consumption.ts
@@ -15,11 +15,11 @@ export default {
   },
   [`${MODULES.EquipmentElectricConsumption}-title-subtext`]: {
     en: `This module allows you to estimate the electrical consumption of the scientific, IT, and other equipment in your unit. The equipment list comes from the equipment inventory carried out by your unit for the faculty. Please fill in the following columns:
-Active use and standby use: Please enter the number of hours each piece of equipment is used per week. It is recommended to make a conservative estimate (not underestimated) to minimize the time required for this task. If your equipement active or standby use is different from the one used by default, please contact anna.kouninamasse%40epfl.ch.
+Active use and standby use: Please enter the number of hours each piece of equipment is used per week. It is recommended to make a conservative estimate (not underestimated) to minimize the time required for this task. If your equipement active or standby use is different from the one used by default, please contact anna.kouninamasse[at]epfl.ch.
 Subclass: Choose the subclass for equipment where this information is necessary.
 Class: Please update the class if the one from your inventory is not appropriate. Note that you will need to apply this change during your next inventory update, as it is not automatically synced through the CO₂ Calculator.`,
     fr: `Ce module permet d'estimer la consommation électrique des équipements scientifiques, IT et autres dans votre unité. La liste de équipements vient de l'inventaire d'équipement effectué par votre unité pour la faculté. Veuillez remplir les colonnes suivantes:
-Usage actif et usage standby: veuillez remplir les heures d'utilisation de chaque équipement par semaine. Il est recommandé de faire une estimation conservatrice (qui n'est pas sous-estimée) pour limiter le temps dédié à cette tâche. Si la puissance moyenne active ou standby de votre équipement est différente de celle utilisée par défaut, merci de contacter anna.kouninamasse%40epfl.ch.
+Usage actif et usage standby: veuillez remplir les heures d'utilisation de chaque équipement par semaine. Il est recommandé de faire une estimation conservatrice (qui n'est pas sous-estimée) pour limiter le temps dédié à cette tâche. Si la puissance moyenne active ou standby de votre équipement est différente de celle utilisée par défaut, merci de contacter anna.kouninamasse[at]epfl.ch.
 Sous-classe: choisissez la sous-classe pour les équipements où cette information est nécessaire.
 Classe: veuillez mettre à jour la classe si celle de votre inventaire n'est pas appropriée. Attention, vous devrez répercuter ce changement lors de votre prochaine mise à jour de l'inventaire, car celle-ci ne se fait pas automatiquement à travers le Calculateur CO₂.`,
   },
