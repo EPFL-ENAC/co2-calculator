54c5eeeab1f3ad5733e7000991db0074d3c6e8f4
Guilbert Pierre <pierre.guilbert@epfl.ch>
2026-01-29 06:43:00 +0100

    chore: add text for module

--- frontend/src/i18n/external_cloud.ts
+++ frontend/src/i18n/external_cloud.ts
@@ -1,64 +1,71 @@
-import { MODULES, MODULES_DESCRIPTIONS } from 'src/constant/modules';
+import { MODULES } from 'src/constant/modules';
 
 export default {
   [MODULES.ExternalCloudAndAI]: {
-    en: 'External Cloud and AI',
-    fr: 'Cloud externe et IA',
+    en: 'External clouds',
+    fr: 'Clouds externes',
   },
-  [MODULES_DESCRIPTIONS.ExternalCloudAndAI]: {
-    en: 'Measure cloud computing emissions from external service providers.',
-    fr: "Mesurez les émissions liées à l'informatique en nuage auprès de fournisseurs externes.",
+  [`${MODULES.ExternalCloudAndAI}-description`]: {
+    en: 'Enter external clouds usage data to estimate the carbon footprint.',
+    fr: "Saisissez vos données relatives à l'utilisation de clouds externes pour estimer leur empreinte carbone.",
   },
+  [`${MODULES.ExternalCloudAndAI}-title-subtext`]: {
+    en: 'This module calculates the carbon footprint associated with the use of cloud services, all features combined. Data entry is manual and can be reviewed and added or deleted by the user.\nSome cells may or may not be editable depending on the type of service selected.',
+    fr: "Ce module calcule l'empreinte carbone liée à l'utilisation de clouds externes, toutes fonctionnalités confondues. La saisie des informations est manuelle. Vous pouvez consulter, ajouter ou supprimer des entrées selon vos besoins.\nCertaines cellules seront ou non éditables en fonction du type de service sélectionné.",
+  },
+  [`${MODULES.ExternalCloudAndAI}-title-tooltip-title`]: {
+    en: 'You can add data one at a time using the Add button below, or upload several entries at once using a file that follows the template.',
+    fr: 'Vous pouvez ajouter les données une par une en utilisant le bouton « Ajouter » ci-dessous, ou importer plusieurs entrées à la fois via un fichier respectant le modèle fourni.',
+  },
+
   // CHARTS external-cloud-and-ai.charts-title
-  [`${MODULES.ExternalCloudAndAI}-charts-title`]: { 
-    en: 'External Cloud and AI Emissions',
-    fr: 'Émissions du cloud externe et de l\'IA',
-   },
+  [`${MODULES.ExternalCloudAndAI}-charts-title`]: {
+    en: 'External Cloud Emissions',
+    fr: 'Émissions du cloud externe',
+  },
   // external-cloud-and-ai.cloud_services_table_title
-  [`${MODULES.ExternalCloudAndAI}.cloud_services_table_title`]: { 
+  [`${MODULES.ExternalCloudAndAI}.cloud_services_table_title`]: {
     en: 'Cloud Services',
     fr: 'Services Cloud',
-   },
-   // external-cloud-and-ai.ai_usage_table_title
-  [`${MODULES.ExternalCloudAndAI}.ai_usage_table_title`]: { 
+  },
+  // external-cloud-and-ai.ai_usage_table_title
+  [`${MODULES.ExternalCloudAndAI}.ai_usage_table_title`]: {
     en: 'AI Usage',
-    fr: 'Utilisation de l\'IA',
-   },
+    fr: "Utilisation de l'IA",
+  },
   // CLOUD SUBMODULE
-  [`${MODULES.ExternalCloudAndAI}.inputs.service_type`]: { 
+  [`${MODULES.ExternalCloudAndAI}.inputs.service_type`]: {
     en: 'Service Type',
     fr: 'Type de service',
-   },
-  [`${MODULES.ExternalCloudAndAI}.inputs.cloud_provider`]: { 
+  },
+  [`${MODULES.ExternalCloudAndAI}.inputs.cloud_provider`]: {
     en: 'Cloud Provider',
     fr: 'Fournisseur de services cloud',
-   },
-  [`${MODULES.ExternalCloudAndAI}.inputs.region`]: { 
+  },
+  [`${MODULES.ExternalCloudAndAI}.inputs.region`]: {
     en: 'Region',
     fr: 'Région',
-   },
-  [`${MODULES.ExternalCloudAndAI}.inputs.spending`]: { 
+  },
+  [`${MODULES.ExternalCloudAndAI}.inputs.spending`]: {
     en: 'Spending',
     fr: 'Dépenses',
-   },
-   // Co2eq is the same in both languages is using common key
+  },
+  // Co2eq is the same in both languages is using common key
   // AI SUBMODULE
-  [`${MODULES.ExternalCloudAndAI}.inputs.ai_provider`]: { 
+  [`${MODULES.ExternalCloudAndAI}.inputs.ai_provider`]: {
     en: 'AI Provider',
-    fr: 'Fournisseur d\'IA',
-   },
-  [`${MODULES.ExternalCloudAndAI}.inputs.ai_use`]: { 
+    fr: "Fournisseur d'IA",
+  },
+  [`${MODULES.ExternalCloudAndAI}.inputs.ai_use`]: {
     en: 'AI Use',
-    fr: 'Utilisation de l\'IA',
-   },
-  [`${MODULES.ExternalCloudAndAI}.inputs.frequency_use_per_day`]: { 
+    fr: "Utilisation de l'IA",
+  },
+  [`${MODULES.ExternalCloudAndAI}.inputs.frequency_use_per_day`]: {
     en: 'Frequency of Use per Day',
-    fr: 'Fréquence d\'utilisation par jour',
-   },
-  [`${MODULES.ExternalCloudAndAI}.inputs.user_count`]: { 
+    fr: "Fréquence d'utilisation par jour",
+  },
+  [`${MODULES.ExternalCloudAndAI}.inputs.user_count`]: {
     en: 'User Count',
-    fr: 'Nombre d\'utilisateurs',
-   },
-
-
+    fr: "Nombre d'utilisateurs",
+  },
 } as const;
