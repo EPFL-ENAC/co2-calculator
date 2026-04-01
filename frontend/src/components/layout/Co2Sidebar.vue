a6b6870e0bdf3ccaaf1ee0acb8e230935703b026
Benjamin <benjaminbotros@proton.me>
2026-04-01 15:20:48 +0200

    feat: add collapsible Co2 sidebar with toggle

--- frontend/src/components/layout/Co2Sidebar.vue
+++ frontend/src/components/layout/Co2Sidebar.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 import { NavItem } from 'src/constant/navigation';
 import { useRouter } from 'vue-router';
 import { useAuthStore } from 'src/stores/auth';
@@ -13,6 +13,7 @@ interface Props {
 defineProps<Props>();
 const router = useRouter();
 const authStore = useAuthStore();
+const collapsed = ref(false);
 
 function navigateToRoute(routeName: string) {
   router.push({ name: routeName });
@@ -37,21 +38,34 @@ function isItemDisabled(item: NavItem): boolean {
 </script>
 
 <template>
-  <q-list class="co2-sidebar">
-    <q-item
-      v-for="item in items"
-      :key="item.routeName"
-      class="co2-sidebar-item"
-      :class="{
-        'co2-sidebar-item--selected':
-          router.currentRoute.value.name === item.routeName,
-      }"
-      :disable="isItemDisabled(item)"
-      clickable
-      @click="!isItemDisabled(item) && navigateToRoute(item.routeName)"
-    >
-      <q-icon :name="item.icon" size="sm" />
-      <q-item-label class="text-body2">{{ $t(item.routeName) }}</q-item-label>
-    </q-item>
-  </q-list>
+  <div class="co2-sidebar" :class="{ 'co2-sidebar--collapsed': collapsed }">
+    <div class="co2-sidebar-toggle" @click="collapsed = !collapsed">
+      <q-icon
+        :name="collapsed ? 'chevron_right' : 'chevron_left'"
+        size="sm"
+      />
+    </div>
+    <q-list class="co2-sidebar-items">
+      <q-item
+        v-for="item in items"
+        :key="item.routeName"
+        class="co2-sidebar-item"
+        :class="{
+          'co2-sidebar-item--selected':
+            router.currentRoute.value.name === item.routeName,
+        }"
+        :disable="isItemDisabled(item)"
+        clickable
+        @click="!isItemDisabled(item) && navigateToRoute(item.routeName)"
+      >
+        <q-icon :name="item.icon" size="sm" />
+        <q-item-label v-show="!collapsed" class="text-body2">{{
+          $t(item.routeName)
+        }}</q-item-label>
+        <q-tooltip v-if="collapsed" anchor="center right" self="center left">
+          {{ $t(item.routeName) }}
+        </q-tooltip>
+      </q-item>
+    </q-list>
+  </div>
 </template>
