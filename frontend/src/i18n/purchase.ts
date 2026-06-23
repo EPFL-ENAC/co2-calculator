4e3ca49541ea5b3b84ca0c8bdedd1d76d5cb4d98
Tomoko Muranaka <tomoko.muranaka@epfl.ch>
2026-06-23 15:30:43 +0200

    Update purchase.ts

--- frontend/src/i18n/purchase.ts
+++ frontend/src/i18n/purchase.ts
@@ -11,18 +11,18 @@ export default {
   },
   [`${MODULES.Purchase}-description`]: {
     en: 'Review annual purchase data and its carbon footprint.',
-    fr: "Vérifiez vos données d'achats annuelles et leur empreintes carbones.",
+    fr: "Vérifiez vos données d'achats annuelles et leur empreinte carbone.",
   },
   [`${MODULES.Purchase}-title-subtext`]: {
-    en: 'This module calculates the carbon footprint of your unit’s purchases on an item-by-item basis using imported data. Purchase entries can be reviewed and added by the user if necessary. Emissions are estimated using spend-based emission factors by default. For specific categories, such as liquid nitrogen, users may enter usage data to apply usage-based emission factors. If you have questions or need clarification regarding specific items, please contact XX.',
-    fr: "Ce module calcule l'empreinte carbone des achats de votre unité, article par article, à partir des données importées. Vous pouvez consulter et ajouter des entrées d'achat selon vos besoins. Les émissions sont estimées par défaut à partir des dépenses. Pour certaines catégories (comme l'azote liquide), vous avez la possibilité de saisir des données d'usage afin d'utiliser des facteurs d'émission spécifiques. Si vous avez des questions ou besoin de clarification concernant vos achats, veuillez contacter XX.",
+    en: "This module calculates the carbon footprint of your unit’s purchases on an item-by-item basis by using procurement data registered in the EPFL's invoice system. You can review the entries and manually add any missing purchases. By default, emissions are estimated using financial spend-based emission factors. However, for specific centralized purchase categories where you can enter usage data, the system will automatically apply usage-based emission factors. Please note that purchases, such that made through internal shops or via units' credit cards, are not currently included in the automatic sync. We highly encourage you to manually fill in these expenses to receive a complete overview for your unit.",
+    fr: "Ce module calcule l'empreinte carbone des achats de votre unité, article par article, en utilisant les données d'approvisionnement enregistrées dans le système EPFL de facturation. Vous pouvez vérifier les entrées et ajouter manuellement les achats manquants. Par défaut, les émissions sont estimées à l'aide de facteurs d'émission basés sur les dépenses financières. Cependant, pour certaines catégories d'achats centralisés où vous pouvez saisir des données d'utilisation, le système appliquera automatiquement des facteurs d'émission basés sur l'utilisation. Veuillez noter qu'actuellement les achats effectués auprès de magasins internes ou via les cartes de crédit des unités, ne remontent pas automatiquement. Nous vous encourageons vivement à renseigner manuellement ces dépenses afin d'obtenir une image de vos achats aussi précise que possible.",
   },
   [`${MODULES.Purchase}-charts-title`]: {
     en: 'Charts',
     fr: 'Graphiques',
   },
   [`${MODULES.Purchase}.inputs.name`]: {
-    en: 'Item Description',
+    en: 'Item description',
     fr: 'Description de l’article',
   },
   [`${MODULES.Purchase}.inputs.purchase_institutional_code`]: {
@@ -30,8 +30,8 @@ export default {
     fr: 'Description UNSPSC',
   },
   [`${MODULES.Purchase}.inputs.purchase_institutional_code-hint`]: {
-    en: 'To identify the corresponding UNSPSC Code, please consult the reference table.',
-    fr: 'Pour identifier le code UNSPSC correspondant, veuillez consulter le tableau de référence.',
+    en: '',
+    fr: '',
   },
   [`${MODULES.Purchase}.inputs.supplier`]: {
     en: 'Supplier',
@@ -42,7 +42,7 @@ export default {
     fr: 'Quantité',
   },
   [`${MODULES.Purchase}.inputs.total_spent_amount`]: {
-    en: 'Total Spent Amount',
+    en: 'Total spent amount',
     fr: 'Montant total dépensé',
   },
   [`${MODULES.Purchase}.inputs.currency`]: {
@@ -54,7 +54,7 @@ export default {
     fr: 'CHF par défaut.',
   },
   [`${MODULES.Purchase}.inputs.annual_consumption`]: {
-    en: 'Annual Consumption',
+    en: 'Annual consumption',
     fr: 'Consommation annuelle',
   },
   [`${MODULES.Purchase}.inputs.unit`]: {
@@ -62,7 +62,7 @@ export default {
     fr: 'Unité',
   },
   [`${MODULES.Purchase}.inputs.coef_to_kg`]: {
-    en: 'Conversion Coefficient to kg CO₂-eq',
+    en: 'Conversion coefficient to kg CO₂-eq',
     fr: 'Coefficient de conversion en kg CO₂-eq',
   },
   [`${MODULES.Purchase}.${SUBMODULE_PURCHASE_TYPES.ScientificEquipmentPurchases}-table-title`]:
@@ -73,7 +73,7 @@ export default {
   [`${MODULES.Purchase}-${SUBMODULE_PURCHASE_TYPES.ScientificEquipmentPurchases}-form-title`]:
     {
       en: 'Add scientific equipment',
-      fr: 'Ajouter un équipement scientifique',
+      fr: 'Ajoutez un équipement scientifique',
     },
   [`${MODULES.Purchase}.${SUBMODULE_PURCHASE_TYPES.ITEquipmentPurchases}-table-title`]:
     {
@@ -83,27 +83,27 @@ export default {
   [`${MODULES.Purchase}-${SUBMODULE_PURCHASE_TYPES.ITEquipmentPurchases}-form-title`]:
     {
       en: 'Add IT equipment',
-      fr: 'Ajouter un équipement informatique',
+      fr: 'Ajoutez un équipement informatique',
     },
   [`${MODULES.Purchase}.${SUBMODULE_PURCHASE_TYPES.ConsumablePurchases}-table-title`]:
     {
-      en: 'Consumables & Accessories ({count})',
+      en: 'Consumables & accessories ({count})',
       fr: 'Consommables et accessoires ({count})',
     },
   [`${MODULES.Purchase}-${SUBMODULE_PURCHASE_TYPES.ConsumablePurchases}-form-title`]:
     {
-      en: 'Add Consumable',
-      fr: 'Ajouter un consommable',
+      en: 'Add consumable',
+      fr: 'Ajoutez un consommable',
     },
   [`${MODULES.Purchase}.${SUBMODULE_PURCHASE_TYPES.BioProductPurchases}-table-title`]:
     {
-      en: 'Biological, Chemical & Gaseous Products ({count})',
+      en: 'Biological, chemical & gaseous products ({count})',
       fr: 'Produits biologiques chimiques et gazeux ({count})',
     },
   [`${MODULES.Purchase}-${SUBMODULE_PURCHASE_TYPES.BioProductPurchases}-form-title`]:
     {
-      en: 'Add Biological/Chemical/Gaseous Product',
-      fr: 'Ajouter un produit biologique/chimique/gazeux',
+      en: 'Add biological, chemical & gaseous product',
+      fr: 'Ajoutez un produit biologique, chimique et gazeux',
     },
   [`${MODULES.Purchase}.${SUBMODULE_PURCHASE_TYPES.ServicePurchases}-table-title`]:
     {
@@ -112,8 +112,8 @@ export default {
     },
   [`${MODULES.Purchase}-${SUBMODULE_PURCHASE_TYPES.ServicePurchases}-form-title`]:
     {
-      en: 'Add Service',
-      fr: 'Ajouter un service',
+      en: 'Add service',
+      fr: 'Ajoutez un service',
     },
   [`${MODULES.Purchase}.${SUBMODULE_PURCHASE_TYPES.VehiclePurchases}-table-title`]:
     {
@@ -122,21 +122,21 @@ export default {
     },
   [`${MODULES.Purchase}-${SUBMODULE_PURCHASE_TYPES.VehiclePurchases}-form-title`]:
     {
-      en: 'Add Vehicle',
-      fr: 'Ajouter un véhicule',
+      en: 'Add vehicle',
+      fr: 'Ajoutez un véhicule',
     },
   [`${MODULES.Purchase}.${SUBMODULE_PURCHASE_TYPES.OtherPurchases}-table-title`]:
     {
-      en: 'Other Purchases ({count})',
+      en: 'Other purchases ({count})',
       fr: 'Autres achats ({count})',
     },
   [`${MODULES.Purchase}-${SUBMODULE_PURCHASE_TYPES.OtherPurchases}-form-title`]:
     {
-      en: 'Add Other Purchase',
-      fr: 'Ajouter un autre achat',
+      en: 'Add other purchase',
+      fr: 'Ajoutez un autre achat',
     },
   [`${MODULES.Purchase}-${SUBMODULE_PURCHASE_TYPES.AdditionalPurchases}`]: {
-    en: 'Additional purchase | Additional purchases',
+    en: 'Centralized purchase | Centralized purchases',
     fr: 'Achats centralisés | achats centralisés',
   },
   [`${MODULES.Purchase}.${SUBMODULE_PURCHASE_TYPES.AdditionalPurchases}-table-title`]:
