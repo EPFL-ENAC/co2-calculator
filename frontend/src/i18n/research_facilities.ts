7d44128fe2e44bd3b3846772e93cff8fd3c78c48
Tomoko Muranaka <tomoko.muranaka@epfl.ch>
2026-06-23 15:47:57 +0200

    Update research_facilities.ts

--- frontend/src/i18n/research_facilities.ts
+++ frontend/src/i18n/research_facilities.ts
@@ -9,16 +9,16 @@ export default {
     fr: 'Infrastructures de recherche EPFL',
   },
   [`${MODULES.ResearchFacilities}-description`]: {
-    en: "Review the use of your unit's research facilities (at EPFL).",
+    en: "Review the use of your unit's research facilities at EPFL.",
     fr: "Examinez l'utilisation des infrastructures de recherche EPFL de votre unité.",
   },
   [`${MODULES.ResearchFacilities}-title-subtext`]: {
-    en: "EPFL provides many research facilities such as cleanrooms, high‑performance computing centers, IT infrastructures and services as well as animal facilities, to name just a few. In total, there are more than 40 research facilities and several research centers that are used by the EPFL community, (as well as by other academic institutions, start-ups, companies, and industries). This sharing of resources is an excellent way to reduce carbon footprint. Review your unit's use of EPFL research infrastructure and/or animal facilities.",
-    fr: "L'EPFL mutualise de nombreux services internes tels que des salles blanches, des centres de calculs de hautes performances, des services informatiques ou des animaleries, pour n'en nommer que quelques-uns. Au total, il y a plus de 40 plateformes et centres de recherches qui sont utilisés par la communauté EPFL (mais également d'autres institutions académiques ainsi que des start-ups, entreprises et industries). Cette mutualisation des ressources est un excellent moyen de réduire l'empreinte carbone. Examinez l'utilisation des infrastructures de recherche et/ou des animaleries EPFL par votre unité.",
+    en: "EPFL provides many research facilities such as cleanrooms, high‑performance computing centers, IT infrastructures and services as well as animal facilities, to name just a few. In total, there are more than 40 research facilities and several research centers that are used by the EPFL community, (as well as by other academic institutions, start-ups, companies, and industries). This sharing of resources is an excellent way to reduce carbon footprint.",
+    fr: "L'EPFL mutualise de nombreux services internes tels que des salles blanches, des centres de calculs de hautes performances, des services informatiques ou des animaleries, pour n'en nommer que quelques-uns. Au total, il y a plus de 40 plateformes et centres de recherches qui sont utilisés par la communauté EPFL (mais également d'autres institutions académiques ainsi que des start-ups, entreprises et industries). Cette mutualisation des ressources est un excellent moyen de réduire l'empreinte carbone.",
   },
   [`${MODULES.ResearchFacilities}.${SUBMODULE_RESEARCH_FACILITIES_TYPES.ResearchFacilities}-table-title`]:
     {
-      en: 'Research Facilities ({count})',
+      en: 'Research facilities ({count})',
       fr: 'Infrastructures de recherche ({count})',
     },
   [`${MODULES.ResearchFacilities}.${SUBMODULE_RESEARCH_FACILITIES_TYPES.AnimalFacilities}`]:
@@ -32,7 +32,7 @@ export default {
       fr: 'Animaleries rongeurs et poissons ({count})',
     },
   [`${MODULES.ResearchFacilities}.inputs.name`]: {
-    en: 'Research Facility',
+    en: 'Research facility',
     fr: 'Infrastructure de recherche',
   },
   [`${MODULES.ResearchFacilities}.inputs.use_unit`]: {
@@ -52,8 +52,8 @@ export default {
     fr: "Nombre d'hébergements",
   },
   [`${MODULES.ResearchFacilities}.inputs.housing_nb-tooltip`]: {
-    en: 'For the mice and fish facilities of the CPG unit, we consider only the annual housing component, and not phenotyping or UDP.',
-    fr: "Pour les animaleries rongeurs et poissons de l'unité CPG, nous ne considérons que la partie hébergement annuel et non le phénotypage ou UDP.",
+    en: "For EPFL's rodent and fish animal facilities, only the animal housing component is considered. The Phenotyping Unit (UDP) and the Transgenesis Platform (TCF) are not included.",
+    fr: "Pour l'utilisation des animaleries rongeurs et poissons à l'EPFL, nous ne considérons que la partie hébergement des animaux et pas ce qui concerne l'unité de phénotypage (UDP) et la plateforme de transgénèse (TCF).",
   },
   fish: {
     en: 'Fish',
