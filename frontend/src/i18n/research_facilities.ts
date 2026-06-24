3a8c9584af308bacda19dfbc26d50e404ca3f1d4
ymarcon <yannick.marcon@epfl.ch>
2026-06-24 12:16:31 +0200

    fix: formatting

--- frontend/src/i18n/research_facilities.ts
+++ frontend/src/i18n/research_facilities.ts
@@ -13,7 +13,7 @@ export default {
     fr: "Examinez l'utilisation des infrastructures de recherche EPFL de votre unité.",
   },
   [`${MODULES.ResearchFacilities}-title-subtext`]: {
-    en: "EPFL provides many research facilities such as cleanrooms, high‑performance computing centers, IT infrastructures and services as well as animal facilities, to name just a few. In total, there are more than 40 research facilities and several research centers that are used by the EPFL community, (as well as by other academic institutions, start-ups, companies, and industries). This sharing of resources is an excellent way to reduce carbon footprint.",
+    en: 'EPFL provides many research facilities such as cleanrooms, high‑performance computing centers, IT infrastructures and services as well as animal facilities, to name just a few. In total, there are more than 40 research facilities and several research centers that are used by the EPFL community, (as well as by other academic institutions, start-ups, companies, and industries). This sharing of resources is an excellent way to reduce carbon footprint.',
     fr: "L'EPFL mutualise de nombreux services internes tels que des salles blanches, des centres de calculs de hautes performances, des services informatiques ou des animaleries, pour n'en nommer que quelques-uns. Au total, il y a plus de 40 plateformes et centres de recherches qui sont utilisés par la communauté EPFL (mais également d'autres institutions académiques ainsi que des start-ups, entreprises et industries). Cette mutualisation des ressources est un excellent moyen de réduire l'empreinte carbone.",
   },
   [`${MODULES.ResearchFacilities}.${SUBMODULE_RESEARCH_FACILITIES_TYPES.ResearchFacilities}-table-title`]:
