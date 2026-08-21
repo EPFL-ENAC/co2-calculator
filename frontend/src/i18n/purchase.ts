cd0eafcf49dbce0e0241ea1c8930588aacfa4d4a
Benjamin <benjaminbotros@proton.me>
2026-08-21 15:04:44 +0200

    fix: correct syntax

--- frontend/src/i18n/purchase.ts
+++ frontend/src/i18n/purchase.ts
@@ -10,7 +10,7 @@ export default {
     fr: 'Données et facteurs communs | Données et facteurs communs',
   },
   [`${MODULES.Purchase}-description`]: {
-    en: 'Review and complete your unit's purchases.',
+    en: `Review and complete your unit's purchases.`,
     fr: "Vérifiez et complétez les données d'achats de votre unité.",
   },
   [`${MODULES.Purchase}-documentation-link`]: {
@@ -18,14 +18,14 @@ export default {
     fr: 'https://epfl-enac.github.io/co2-calculator-user-doc/fr/purchases/',
   },
   [`${MODULES.Purchase}-title-subtext`]: {
-    en: "This module helps you estimate your unit’s purchasing carbon footprint item by item, based on procurement records from the invoicing system. By default, each item is analyzed by its specific category to apply the corresponding spend-based emission factor.
+    en: `This module helps you estimate your unit’s purchasing carbon footprint item by item, based on procurement records from the invoicing system. By default, each item is analyzed by its specific category to apply the corresponding spend-based emission factor.
 
 - Centralized purchases: For these categories, you can enter physical consumption metrics (quantities used). The system will automatically apply activity-based emission factors to calculate the corresponding carbon footprint.
   
 - Credit card purchases: These expenses are not currently imported automatically. Please enter your unit’s credit card purchases to ensure your assessment is as accurate as possible.
   
-- Internal store purchases: Purchases made at chemical stores (ISIC-CHSP) should not be entered here; they can be viewed directly in the EPFL research facilities module.",
-    fr: "Ce module vous aide à estimer l'empreinte carbone liée aux achats de votre unité, article par article, à partir des données d'approvisionnement enregistrées dans le système de facturation.
+- Internal store purchases: Purchases made at chemical stores (ISIC-CHSP) should not be entered here; they can be viewed directly in the EPFL research facilities module.`,
+    fr: `Ce module vous aide à estimer l'empreinte carbone liée aux achats de votre unité, article par article, à partir des données d'approvisionnement enregistrées dans le système de facturation.
 
 Par défaut, chaque article est analysé selon sa typologie précise afin de lui attribuer le facteur d'émission monétaire correspondant.
 
@@ -33,7 +33,7 @@ Par défaut, chaque article est analysé selon sa typologie précise afin de lui
     
 - Achats par carte de crédit : ces dépenses ne remontant pas automatiquement actuellement. Veuillez saisir vos achats par la carte de crédit de votre unité afin d'obtenir un bilan aussi précis que possible.
     
-- Achats en magasins internes : Les achats effectués aux magasins de chimie (ISIC-CHSP) ne doivent pas être saisis ici ; ils peuvent être consultés directement dans le module Infrastructures de recherche EPFL.",
+- Achats en magasins internes : Les achats effectués aux magasins de chimie (ISIC-CHSP) ne doivent pas être saisis ici ; ils peuvent être consultés directement dans le module Infrastructures de recherche EPFL.`,
   },
   [`${MODULES.Purchase}-charts-title`]: {
     en: 'Charts',
